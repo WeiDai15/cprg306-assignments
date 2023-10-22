79dd776a2845cf1b2420f64f56b7c3cf17a27f9c
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-10-21 22:49:35 -0600

    Update page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import StudentInfo from '../../StudentInfo'; // Import the StudentInfo component
+import StudentInfo from '../../StudentInfo';
 
 const Week2Page = () => {
   return (
     <div>
       <h1>My Shopping List</h1>
-      <StudentInfo /> {/* Render the StudentInfo component */}
+      <StudentInfo name="Wei Dai" course="CPRG306 D"></StudentInfo>
     </div>
   );
 };
