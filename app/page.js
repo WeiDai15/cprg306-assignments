740044affa157ee59c33d86ff3abea265a456391
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-10-21 22:45:40 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home(){
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className='z-10 max-w-5x1 w-full items-center justify-between font-mono text-sm'>
                 <hi>CPRG 306: Web Development 2 - Assignments</hi>
-                <StudentInfo/>
+                <StudentInfo name="Wei Dai" course="CPRG306 D"></StudentInfo>
                 <div><link href='week2'>week2</link></div>
                 <div><link href='week3'>week3</link></div>
                 <div><link href='week4'>week4</link></div>
