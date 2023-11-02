24ad233120a343865ae4dbfe2d6a7dd9ca7b810f
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-11-01 21:42:43 -0600

    week6

---
+++ app/week6/page.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import NewItem from './new-item'; 
+import ItemList from './item-list'; 
+import itemsData from './items.json'; 
+
+const Page = () => {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  return (
+    <main className="max-w-screen-sm mx-auto p-4">
+      <h1 className="text-3xl font-semibold mb-4">Shopping List</h1>
+
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
+      
+    </main>
+  );
+};
+
+export default Page;
