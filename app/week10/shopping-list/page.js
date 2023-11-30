6269aae66f6ff2244d3529ac51d5a434d6b1c8c4
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-11-30 14:46:46 -0700

    Update page.js

--- app/week10/shopping-list/page.js
+++ app/week10/shopping-list/page.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
 import NewItem from './new-item'; 
 import ItemList from './item-list'; 
-import itemsData from './items.json'; 
+import MealIdeas from "./meal-ideas";
+
+import { getItems, addItem } from "./_services/shopping-list-service";
+import { useEffect } from "react";
 
 const Page = () => {
   const [items, setItems] = useState(itemsData);
 
-  const handleAddItem = (newItem) => {
-    setItems([...items, newItem]);
-  };
+  const handleAddItem = async (newItem) => {
+  try {
+    const newItemId = await addItem(user.uid, newItem);
+    setItems((prevItems) => [...prevItems, { id: newItemId, ...newItem }]);
+  } catch (error) {
+    console.error("Error adding item:", error);
+  }
+};
+
+  
+  const loadItems = async () => {
+  try {
+    const fetchedItems = await getItems(user.uid);
+    setItems(fetchedItems);
+  } catch (error) {
+    console.error("Error fetching items:", error);
+  }
+};
+  
+  useEffect(() => {
+  if (user && user.uid) {
+    loadItems();
+    }
+  }, [user?.uid]);
 
   return (
     <main className="max-w-screen-sm mx-auto p-4">
